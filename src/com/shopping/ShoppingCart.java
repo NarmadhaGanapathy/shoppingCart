1eb150debc424ff99e5441e71077109bd88584ab
Narmadha Ganapathy <selvanarmadha.ganapathy@gmail.com>
2019-09-22 14:15:48 -0400

    Initial idea for the shopping cart

---
+++ src/com/shopping/ShoppingCart.java
@@ -0,0 +1,14 @@
+package com.shopping;
+/**
+ * Created by Narmadha Ganapathy
+ ***/
+public class ShoppingCart {
+
+    public static void main(String[] args) {
+
+	// get input from the user as scanned item and calculate the total price.
+    // Use a map to maintain the price for the items(apple, orange)
+    // calculate the total price  once the user is done scanning
+
+    }
+}
